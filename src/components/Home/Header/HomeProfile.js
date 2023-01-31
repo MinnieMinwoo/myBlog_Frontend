5c040864be11d432810f0af46745fca507fdbaa2
MinnieMinwoo <nhs075241@gmail.com>
2023-01-31 17:37:40 +0900

    add profile, write css and logout logic

--- src/components/Home/Header/HomeProfile.js
+++ src/components/Home/Header/HomeProfile.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { loginData } from "../../../states/LoginState";
+import { authService } from "../../../firebase";
+import { signOut } from "firebase/auth";
 import styled from "styled-components";
 
 const ProfileBox = styled.div`
@@ -16,21 +21,61 @@ const ProfileButton = styled.button`
     background-repeat: no-repeat;
 `;
 
+const ButtonContainer = styled.div`
+    position: absolute;
+    transform: translateX(-34px);
+    @media (max-width: 1080px) {
+        transform: translateX(-64px);
+    }
+    margin-top: 5px;
+`;
+const RouteButton = styled.button`
+    display: block;
+    box-sizing: content-box;
+    width: 96px;
+    padding: 0;
+    border: 1px solid #eee;
+    background-color: #fff;
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+    font-size: 14px;
+    line-height: 35px;
+    color: #777;
+`;
+
 const HomeProfile = () => {
     const [isHidden, setIsHidden] = useState(true);
-    const onClick = () => {
-        console.log(isHidden);
+    const navigate = useNavigate();
+    const setUserData = useSetRecoilState(loginData);
+    const onToggle = () => {
         setIsHidden((prev) => !prev);
+    };
+
+    const onLogout = async () => {
+        try {
+            await signOut(authService);
+            setUserData({
+                isLoggedIn: false,
+            });
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+        }
     };
     return (
         <ProfileBox className="HomeProfile">
-            <ProfileButton onClick={onClick} />
+            <ProfileButton onClick={onToggle} />
             {isHidden ? null : (
-                <div>
-                    <Link to="write">글쓰기</Link>
-                    <p>설정</p>
-                    <p>로그아웃</p>
-                </div>
+                <ButtonContainer>
+                    <RouteButton as={Link} to="write">
+                        글쓰기
+                    </RouteButton>
+                    <RouteButton as={Link} to="write">
+                        설정
+                    </RouteButton>
+                    <RouteButton onClick={onLogout}>로그아웃</RouteButton>
+                </ButtonContainer>
             )}
         </ProfileBox>
     );
--- src/routes/Write.js
+++ src/routes/Write.js
@@ -1,13 +1,54 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MDEditor from "@uiw/react-md-editor";
 import { dbService } from "../firebase";
 import { addDoc, collection } from "firebase/firestore";
 import { useRecoilValue } from "recoil";
 import { loginData } from "../states/LoginState";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    @media (min-width: 1080px) {
+        width: 80%;
+        margin-left: 10%;
+    }
+`;
+
+const DocTitle = styled.h1`
+    border-bottom: 1px solid #eee;
+    padding-bottom: 22px;
+    color: #333;
+`;
+
+const PostTitle = styled.input`
+    color: #777;
+    border: 0;
+    border-bottom: 1px solid #eee;
+    &:focus {
+        outline: none;
+        border-bottom: 2px solid #777;
+    }
+    font-size: 36px;
+    margin-bottom: 20px;
+`;
+
+const Submit = styled.input`
+    width: 88px;
+    margin: 0;
+    margin-top: 20px;
+    line-height: 38px;
+    border-radius: 20px;
+    float: right;
+    background-color: #000;
+    color: #fff;
+    border: 1px solid #000;
+`;
 
 const Write = () => {
-    const [title, setTitle] = useState("Title");
+    const [title, setTitle] = useState("");
     const [postData, setPostData] = useState("**Write your post**");
     const navigate = useNavigate();
     const userData = useRecoilValue(loginData);
@@ -36,26 +77,29 @@ const Write = () => {
         navigate("/");
     };
     return (
-        <div>
-            <h3>Write your Story</h3>
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    placeholder="Write your title"
-                    value={title}
-                    onChange={onChange}
-                    required
-                />
-                <MDEditor
-                    data-color-mode="light"
-                    minHeight="500px"
-                    height={"100%"}
-                    value={postData}
-                    onChange={setPostData}
-                />
-                <input type="submit" value="Write up" />
-            </form>
-        </div>
+        <Container>
+            <header>
+                <DocTitle>Write your Story</DocTitle>
+            </header>
+            <section>
+                <form onSubmit={onSubmit}>
+                    <PostTitle
+                        type="text"
+                        placeholder="Write post title"
+                        value={title}
+                        onChange={onChange}
+                        required
+                    />
+                    <MDEditor
+                        data-color-mode="light"
+                        height="calc(100vh - 260px)"
+                        value={postData}
+                        onChange={setPostData}
+                    />
+                    <Submit type="submit" value="Write up" />
+                </form>
+            </section>
+        </Container>
     );
 };
 export default Write;
