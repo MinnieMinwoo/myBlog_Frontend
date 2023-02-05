628cc38de8bf478676efd3872195b6d02cf23a8f
MinnieMinwoo <nhs075241@gmail.com>
2023-02-05 18:54:12 +0900

    fix font import

--- src/styles/Globalstyle.ts
+++ src/styles/Globalstyle.ts
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
-
+@font-face {
+    font-family: "Noto Sans KR";
+    src: url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+}
 * {
     font-family: 'Noto Sans KR', sans-serif;
     margin: 0;
