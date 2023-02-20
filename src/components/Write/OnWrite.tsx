593fd46c05d25793161e1f59f05d7664b947eeef
MinnieMinwoo <nhs075241@gmail.com>
2023-02-20 11:43:56 +0900

    add upload image logic

--- src/components/Write/OnWrite.tsx
+++ src/components/Write/OnWrite.tsx
@@ -1,10 +1,20 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, DragEvent, useState, useRef } from "react";
 import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { loginData } from "../../states/LoginState";
+import { uuidv4 } from "@firebase/util";
 import MDEditor from "@uiw/react-md-editor";
 import { Button, Container, Navbar, Col } from "react-bootstrap";
 import styled from "styled-components";
 
+import { uploadImg } from "../../logic/getSetImage";
 import blogIcon from "../../assets/images/logo.png";
+
+const OnDragCheck = styled.div`
+  &.Drag {
+    opacity: 0.3;
+  }
+`;
 
 const WriteContainer = styled(Col)`
   margin-top: 20px;
@@ -58,6 +68,10 @@ interface Props {
 }
 
 const OnWrite = ({ isEdit, postContent, setPostContent, onPreview }: Props) => {
+  const inputRef = useRef<HTMLDivElement>(null);
+  const userData = useRecoilValue(loginData);
+  const [isDragging, setIsDragging] = useState(false);
+
   const onTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const {
       target: { value },
@@ -68,8 +82,45 @@ const OnWrite = ({ isEdit, postContent, setPostContent, onPreview }: Props) => {
     }));
   };
 
+  const onDragEnter = (event: DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsDragging(true);
+  };
+  const onDragLeave = (event: DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsDragging(false);
+  };
+  const onDragOver = (event: DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.dataTransfer.files) {
+      setIsDragging(true);
+    }
+  };
+  const onDrop = async (event: DragEvent) => {
+    event.preventDefault(); // 브라우저 이벤트 방지
+    event.stopPropagation(); // 부모 이벤트 방지
+    const textarea = inputRef.current?.children[0]?.children[1]?.children[0]?.children[0]
+      ?.children[1] as HTMLTextAreaElement; // Editor에서 직접 ref 사용시 속성 정의 에러발생
+    if (!textarea) return;
+    const files = event.dataTransfer.files[0];
+    if (!files) return;
+    const imageLink = await uploadImg(files, `$thumbnail/${userData.uid}/${uuidv4()}`);
+    const currentText = postContent.postData;
+    const textCursor = textarea.selectionStart;
+    setPostContent((prev) => ({
+      ...prev,
+      postData: `${currentText.slice(0, textCursor)}![](${imageLink})${currentText.slice(
+        textCursor
+      )}`,
+    }));
+    setIsDragging(false);
+  };
+
   return (
-    <>
+    <OnDragCheck className={isDragging ? "OnWrite Drag" : "OnWrite"}>
       <header>
         <Navbar bg="light">
           <Container>
@@ -88,7 +139,13 @@ const OnWrite = ({ isEdit, postContent, setPostContent, onPreview }: Props) => {
         xxl={{ span: 6, offset: 3 }}
         className="OnWrite"
       >
-        <section>
+        <section
+          onDragEnter={onDragEnter}
+          onDragLeave={onDragLeave}
+          onDragOver={onDragOver}
+          onDrop={onDrop}
+        >
+          <input hidden type="file" accept="image/*" />
           <PostTitle
             placeholder="Write post title"
             value={postContent.title}
@@ -96,20 +153,22 @@ const OnWrite = ({ isEdit, postContent, setPostContent, onPreview }: Props) => {
             onChange={onTitleChange}
             required
           />
-          <Editor
-            data-color-mode="light"
-            value={postContent.postData}
-            onChange={(value = "") => {
-              setPostContent((prev) => ({
-                ...prev,
-                postData: value,
-              }));
-            }}
-          />
+          <div ref={inputRef}>
+            <Editor
+              data-color-mode="light"
+              value={postContent.postData}
+              onChange={(value = "") => {
+                setPostContent((prev) => ({
+                  ...prev,
+                  postData: value,
+                }));
+              }}
+            />
+          </div>
           <Submit onClick={onPreview}>{isEdit ? "Edit" : "Write up"}</Submit>
         </section>
       </WriteContainer>
-    </>
+    </OnDragCheck>
   );
 };
 
