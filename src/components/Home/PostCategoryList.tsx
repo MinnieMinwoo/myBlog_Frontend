a3fdf0c882ef778a70f7f4a15dae474375693d65
MinnieMinwoo <nhs075241@gmail.com>
2023-03-02 11:39:24 +0900

    save works

--- src/components/Home/PostCategoryList.tsx
+++ src/components/Home/PostCategoryList.tsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
-import { Card, Col, Stack, Button } from "react-bootstrap";
+import { useRecoilValue } from "recoil";
+import { loginData } from "../../states/LoginState";
+import { Card, Col, Stack, Button, Form } from "react-bootstrap";
 import styled from "styled-components";
 
 import { getUserUID } from "../../logic/getSetUserInfo";
-import { getCategoryData } from "../../logic/getSetCategoryInfo";
+import { getCategoryData, setMainCategoryData } from "../../logic/getSetCategoryInfo";
+import { useModal } from "../../states/ModalState";
+
 import altImage from "../../assets/images/altThumbnail.jpg";
+import AlertModal from "../Share/AlertModal";
 
 const MainContainer = styled(Stack)`
   padding: 0 30px;
@@ -57,9 +62,12 @@ const CategoryContainer = styled(Card)`
 `;
 
 const PostCategoryList = () => {
+  const userData = useRecoilValue(loginData);
   const [isEdit, setIsEdit] = useState(false);
-  const params = useParams();
   const [categoryData, setCategoryData] = useState<CategoryData[]>([]);
+  const categoryRef = useRef<HTMLInputElement>(null);
+  const params = useParams();
+  const { openModal } = useModal();
 
   useEffect(() => {
     if (!params.userID) throw console.log("no params");
@@ -77,13 +85,86 @@ const PostCategoryList = () => {
     event.currentTarget.src = altImage;
   };
 
+  const inputForm = (defaultValue = "") => {
+    return (
+      <Form>
+        <Form.Label>Write down the category name.</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="enter category name"
+          defaultValue={defaultValue}
+          ref={categoryRef}
+          required
+        />
+      </Form>
+    );
+  };
+
+  const onAddMainCategory = () => {
+    openModal(
+      "Add category",
+      inputForm(),
+      async () => {
+        if (!categoryRef.current?.value || !userData.uid) return;
+        const category = categoryRef.current?.value;
+        await setMainCategoryData(category, userData.uid);
+        setCategoryData((prev) => [
+          ...prev,
+          {
+            mainField: category,
+            subField: [],
+            thumbnailLink: [],
+          },
+        ]);
+        await getCategoryData(userData.uid);
+      },
+      true
+    );
+  };
+
+  const onAddSubCategory = () => {
+    openModal("Add category", inputForm(), () => {}, true);
+  };
+
+  const onEditMainCategory = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!(event.target as HTMLButtonElement).id) return;
+    const targetId = (event.target as HTMLButtonElement).id;
+    const mainCategory = categoryData[Number(targetId[0])].mainField;
+    openModal("Edit category", inputForm(mainCategory), () => {}, true);
+  };
+
+  const onEditSubCategory = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!(event.target as HTMLButtonElement).id) return;
+    const targetId = (event.target as HTMLButtonElement).id.split(",");
+    const subCategory = categoryData[Number(targetId[0])].subField[Number(targetId[1])];
+    openModal("Edit category", inputForm(subCategory), () => {}, true);
+  };
+
+  const onMainCategoryDelete = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!(event.target as HTMLButtonElement).id) return;
+    const targetId = (event.target as HTMLButtonElement).id;
+    openModal("Warning", "If you really want to delete?", () => {}, true, "danger");
+  };
+
+  const onSubCategoryDelete = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!(event.target as HTMLButtonElement).id) return;
+    const targetId = (event.target as HTMLButtonElement).id;
+    openModal("Warning", "If you really want to delete?", () => {}, true, "danger");
+  };
+
   return (
     <MainContainer className="PostCategoryList">
+      <AlertModal />
       <HeaderBox>
         <Stack direction="horizontal" gap={1}>
           <h2>{"Categories"}</h2>
           <span className="text-primary">({categoryData.length})</span>
-          <Button className="ms-auto" onClick={onEdit}>
+          {isEdit ? (
+            <Button className="ms-auto" variant="outline-primary" onClick={onAddMainCategory}>
+              Add
+            </Button>
+          ) : null}
+          <Button className={isEdit ? "" : "ms-auto"} onClick={onEdit}>
             {isEdit ? "Complete" : "Edit"}
           </Button>
         </Stack>
@@ -98,10 +179,24 @@ const PostCategoryList = () => {
                   <span className="text-secondary">({data.subField.length})</span>
                   {isEdit ? (
                     <>
-                      <Button variant="outline-primary" className="ms-auto">
+                      <Button
+                        id={`${id},1`}
+                        variant="outline-primary"
+                        className="ms-auto"
+                        onClick={onAddSubCategory}
+                      >
                         Add
                       </Button>
-                      <Button variant="outline-danger">Delete</Button>
+                      <Button
+                        id={`${id},2`}
+                        variant="outline-secondary"
+                        onClick={onEditMainCategory}
+                      >
+                        Edit
+                      </Button>
+                      <Button id={`${id},3`} variant="danger" onClick={onMainCategoryDelete}>
+                        Delete
+                      </Button>
                     </>
                   ) : null}
                 </Stack>
@@ -113,11 +208,20 @@ const PostCategoryList = () => {
                       <Card.Img src={data.thumbnailLink[index]} onError={onError} alt="Thumbnail" />
                       <Card.Body>
                         <Card.Title>{`${subData}`}</Card.Title>
-                        <Stack direction="horizontal">
-                          <Button variant="outline-secondary" hidden={!isEdit}>
+                        <Stack direction="horizontal" hidden={!isEdit}>
+                          <Button
+                            id={`${id},${index},1`}
+                            variant="outline-secondary"
+                            onClick={onEditSubCategory}
+                          >
                             ✎
                           </Button>
-                          <Button variant="outline-warning" className="ms-auto" hidden={!isEdit}>
+                          <Button
+                            id={`${id},${index},2`}
+                            variant="outline-danger"
+                            className="ms-auto"
+                            onClick={onSubCategoryDelete}
+                          >
                             🗑️
                           </Button>
                         </Stack>
--- src/logic/getSetCategoryInfo.ts
+++ src/logic/getSetCategoryInfo.ts
@@ -1,6 +1,7 @@
-import { doc, getDoc } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, updateDoc, setDoc } from "firebase/firestore";
 import { dbService } from "./firebase";
 
+/** Get Category Data by user uid*/
 export const getCategoryData = async (uid: string) => {
   const categoryRef = doc(dbService, `users/${uid}/category`, uid);
   try {
@@ -22,5 +23,20 @@ export const getCategoryData = async (uid: string) => {
   } catch (error) {
     console.log(error);
     return [];
+  }
+};
+
+/** Add Main Category Data*/
+export const setMainCategoryData = async (name: string, uid: string) => {
+  try {
+    const categoryRef = doc(dbService, `users/${uid}/category`, `${uid}`);
+    const subCategoryRef = doc(dbService, `users/${uid}/category`, `${name}`);
+    await updateDoc(categoryRef, { order: arrayUnion(name) });
+    await setDoc(subCategoryRef, {
+      subfield: [],
+      thumbnailLink: [],
+    });
+  } catch (error) {
+    throw error;
   }
 };
