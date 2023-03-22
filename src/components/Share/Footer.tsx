a8bde0ba4173b534a1795d86f3af0106885a480c
MinnieMinwoo <nhs075241@gmail.com>
2023-03-22 11:38:23 +0900

    fix css to bootstrap

--- src/components/Share/Footer.tsx
+++ src/components/Share/Footer.tsx
@@ -1,22 +1,18 @@
 import React from "react";
-import { Navbar, Container } from "react-bootstrap";
-import styled from "styled-components";
-
-const TextBox = styled.p`
-  margin: 10px 0;
-  font-size: 14px;
-  color: #777;
-`;
 
 const Footer = () => {
   return (
     <footer className="HomeFooter">
-      <Navbar bg="light">
-        <Container>
-          <TextBox> 2023 My own blog project</TextBox>
-          <TextBox> © Snowcat</TextBox>
-        </Container>
-      </Navbar>
+      <nav className="navbar bg-light">
+        <div className="container">
+          <p className="my-1" style={{ fontSize: "14px", color: "#777" }}>
+            2023 My own blog project
+          </p>
+          <p className="my-1" style={{ fontSize: "14px", color: "#777" }}>
+            © SnowCat
+          </p>
+        </div>
+      </nav>
     </footer>
   );
 };
