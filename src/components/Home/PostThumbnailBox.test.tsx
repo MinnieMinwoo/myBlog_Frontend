46b7794b192b4e2921fa36de5bb72f76b4c7df24
MinnieMinwoo <nhs075241@gmail.com>
2023-03-28 12:34:53 +0900

    add post thumbnail box test code

---
+++ src/components/Home/PostThumbnailBox.test.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { cleanup, render, screen } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
+import PostThumbnailBox from "./PostThumbnailBox";
+import userEvent from "@testing-library/user-event";
+
+describe("Post thumbnailBox test", () => {
+  const postData = [
+    {
+      id: "test1",
+      createdAt: 1621814400000, //2021-5-24
+      createdBy: "testUser",
+      category: [],
+      tag: "",
+      thumbnailData: "test message1",
+      thumbnailImageURL: "",
+      title: "test1",
+    },
+    {
+      id: "test2",
+      createdAt: 1664582400000, //2022-10-1
+      createdBy: "testUser",
+      category: [],
+      tag: "",
+      thumbnailData: "test message2",
+      thumbnailImageURL: "",
+      title: "test2",
+    },
+    {
+      id: "test3",
+      createdAt: 1676419200000, //2023-2-15
+      createdBy: "testUser",
+      category: [],
+      tag: "",
+      thumbnailData: "test message3",
+      thumbnailImageURL: "https://picsum.photos/200/300",
+      title: "test3",
+    },
+  ] as PostData[];
+
+  afterEach(() => cleanup);
+
+  it("Single rendering test with image", () => {
+    render(
+      <MemoryRouter>
+        <RecoilRoot>
+          <PostThumbnailBox postList={[postData[2]]} />
+        </RecoilRoot>
+      </MemoryRouter>
+    );
+    expect(screen.getByText("test3")).toBeInTheDocument();
+    expect(screen.getByText("test message3")).toBeInTheDocument();
+    expect(screen.getByText("2023. 02. 15")).toBeInTheDocument();
+    expect(screen.getByRole("img")).toBeInTheDocument();
+  });
+
+  it("Single rendering test without image", () => {
+    render(
+      <MemoryRouter>
+        <RecoilRoot>
+          <PostThumbnailBox postList={[postData[1]]} />
+        </RecoilRoot>
+      </MemoryRouter>
+    );
+    expect(screen.getByText("test2")).toBeInTheDocument();
+    expect(screen.getByText("test message2")).toBeInTheDocument();
+    expect(screen.getByText("2022. 10. 01")).toBeInTheDocument();
+    expect(screen.queryByRole("img")).not.toBeInTheDocument();
+  });
+
+  it("Multiple rendering test", () => {
+    render(
+      <MemoryRouter>
+        <RecoilRoot>
+          <PostThumbnailBox postList={postData} />
+        </RecoilRoot>
+      </MemoryRouter>
+    );
+    expect(screen.getByText("test2")).toBeInTheDocument(); //test2
+    expect(screen.getByText("test message3")).toBeInTheDocument(); //test3
+    expect(screen.getByText("2021. 05. 24")).toBeInTheDocument(); //test1
+  });
+
+  it("anchor test", () => {
+    render(
+      <MemoryRouter>
+        <RecoilRoot>
+          <PostThumbnailBox postList={postData} />
+        </RecoilRoot>
+      </MemoryRouter>
+    );
+    const component = screen.getAllByRole("link");
+    component.forEach((element, index) => {
+      expect(element).toHaveAttribute("href", `/home/undefined/test${index + 1}`); //undefined because not set recoilState
+    });
+  });
+});
--- src/components/Home/PostThumbnailBox.tsx
+++ src/components/Home/PostThumbnailBox.tsx
@@ -2,72 +2,44 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { isLoadingData } from "../../states/LoadingState";
-import { Stack, Image } from "react-bootstrap";
-import styled from "styled-components";
 
 import getDate from "../../logic/getDate";
-
-const Preview = styled.div`
-  width: 100%;
-  &&:hover {
-    h3 {
-      text-decoration: underline;
-    }
-  }
-  a {
-    padding: 30px 0;
-    border-top: 1px solid #eee;
-    display: flex;
-    text-decoration-line: none;
-  }
-  h3 {
-    cursor: pointer;
-    overflow: hidden;
-    color: #111;
-    font-weight: 600;
-  }
-  img {
-    width: 100%;
-    margin-bottom: 15px;
-    aspect-ratio: 16 / 9;
-    object-fit: cover;
-    object-position: center center;
-  }
-  p {
-    word-break: break-all;
-    cursor: pointer;
-    color: #555;
-  }
-  span {
-    cursor: pointer;
-    color: #999;
-    font-size: 14px;
-  }
-`;
 
 const PostThumbnailBox = ({ postList }: { postList: PostData[] }) => {
   const param = useParams();
   const isLoading = useRecoilValue(isLoadingData);
   return (
-    <article className="PostItemList" hidden={isLoading}>
-      <Stack>
-        {postList.map((post) => (
-          <Preview key={post.id}>
-            <Link className="PostItem" key={post.id} to={`/home/${param.userID}/${post.id}`}>
-              <Stack gap={1}>
-                {post.thumbnailImageURL !== "" ? (
-                  <div>
-                    <Image fluid={true} src={post.thumbnailImageURL} alt="post" />
-                  </div>
-                ) : null}
-                <h3>{post.title}</h3>
-                <p>{post.thumbnailData}</p>
-                <span>{getDate(post.createdAt)}</span>
-              </Stack>
-            </Link>
-          </Preview>
-        ))}
-      </Stack>
+    <article className="PostItemList vstack" hidden={isLoading}>
+      {postList.map((post) => (
+        <div className="w-100" key={post.id}>
+          <Link
+            className="PostItem px-0 py-4 d-flex text-decoration-none"
+            style={{ borderTop: "1px solid #eee" }}
+            key={post.id}
+            to={`/home/${param.userID}/${post.id}`}
+          >
+            <div className="vstack gap-1">
+              {post.thumbnailImageURL !== "" ? (
+                <div>
+                  <img
+                    className="img-fluid w-100 mb-3"
+                    style={{ aspectRatio: "16 / 9", objectFit: "cover" }}
+                    src={post.thumbnailImageURL}
+                    alt="post"
+                  />
+                </div>
+              ) : null}
+              <h3 className="overflow-hidden fw-semibold" style={{ color: "#111" }}>
+                {post.title}
+              </h3>
+              <p className="mb-1 text-break" style={{ color: "#555" }}>
+                {post.thumbnailData}
+              </p>
+              <span style={{ color: "#999", fontSize: "14px" }}>{getDate(post.createdAt)}</span>
+            </div>
+          </Link>
+        </div>
+      ))}
     </article>
   );
 };
