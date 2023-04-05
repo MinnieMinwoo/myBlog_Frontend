44f41e23fd416d5f7602eafc8e3621854211f8e3
MinnieMinwoo <nhs075241@gmail.com>
2023-04-05 09:13:19 +0900

    fix upload bug

--- src/logic/getSetImage.ts
+++ src/logic/getSetImage.ts
@@ -2,20 +2,20 @@ import { storageService } from "./firebase";
 import { ref, uploadString, getDownloadURL, deleteObject } from "firebase/storage";
 
 export const uploadImg = async (data: File, saveUrl: string): Promise<string> => {
+  const reader = new FileReader();
   return new Promise((resolve, reject) => {
-    try {
-      const reader = new FileReader();
-      reader.readAsDataURL(data);
-      reader.onloadend = async (event) => {
-        const imgData = event.target?.result as string;
-        const imageRef = ref(storageService, saveUrl);
+    reader.readAsDataURL(data);
+    reader.onloadend = async (event) => {
+      const imgData = event.target?.result as string;
+      const imageRef = ref(storageService, saveUrl);
+      try {
         const uploadRes = await uploadString(imageRef, imgData, "data_url");
         const imageUrl = await getDownloadURL(uploadRes.ref);
         resolve(imageUrl);
-      };
-    } catch (error) {
-      reject(error);
-    }
+      } catch (error) {
+        reject(error);
+      }
+    };
   });
 };
 
