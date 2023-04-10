0ae541974274118b0bc8e64383b5882e748ca957
MinnieMinwoo <nhs075241@gmail.com>
2023-04-10 09:02:37 +0900

    add test code

---
+++ src/logic/authSetting.test.ts
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment node
+ */
+
+import { FirebaseError, initializeApp } from "firebase/app";
+import * as authSetting from "./authSetting";
+
+describe("auth seting test", () => {
+  beforeAll(async () => {
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_DATABASE_URL,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+      appId: process.env.REACT_APP_APP_ID,
+    };
+
+    initializeApp(firebaseConfig);
+  });
+
+  afterAll(async () => {});
+
+  it("sign in / sign out test", async () => {
+    await expect(
+      authSetting.signInEmail(process.env.TEST_EMAIL as string, process.env.TEST_PASSWORD as string)
+    ).resolves.toBe("Minnie");
+    await authSetting.signOutUser();
+
+    await expect(authSetting.signInEmail("1", "2")).rejects.toHaveProperty(
+      "code",
+      "auth/invalid-email"
+    );
+  });
+});
